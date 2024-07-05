353dde2574d332a0c810bcf37fc45ab262927741
Luna <git@l4.pm>
2024-07-04 23:46:55 -0300

    fix content-type on svlogtail client

--- clients/svlogtail.sh
+++ clients/svlogtail.sh
@@ -5,6 +5,6 @@ set -eux
 hostname=$(hostname)
  
 tail -Fq -n0 /var/log/socklog/*/current | while read -r line; do
-    curl -H "Fog-Hostname: $hostname" -d "$line" -X POST localhost:9741/log
+    curl -H "Fog-Hostname: $hostname" -H 'content-type: text/plain' -d "$line" -X POST localhost:9741/api/v1/log
 done
 
