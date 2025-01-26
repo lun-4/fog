9900688e010d9328bf5f6e286e560370ffb735b2
Luna <git@l4.pm>
2025-01-25 21:34:10 -0300

    fix typo

--- server/lib/fog/application.ex
+++ server/lib/fog/application.ex
@@ -7,7 +7,7 @@ defmodule Fog.Application do
 
   @impl true
   def start(_type, _args) do
-    {:ok, _} = File.mkdir_p(Fog.LogStore.tmp_path())
+    :ok = File.mkdir_p(Fog.LogStore.tmp_path())
 
     children = [
       FogWeb.Telemetry,
